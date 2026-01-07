3a5ecd6f3574ad4ebf9314a31b92d3a7cf7a41bc
zhongfangyu <zhongfangyu@wondersgroup.com>
2026-01-07 17:06:01 +0800

    add: svgIcon组件，进行全局注册

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,15 @@ import App from './App.vue'
 import router from './router'
 // 引入pinia
 import { createPinia } from 'pinia'
+// 引入 element-plus
+import  ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+
+// svg插件需要配置代码
+import 'virtual:svg-icons-register'
+
+// 引入自定义插件对象：注册整个项目全局组件
+import gloablComponent from '@/components'
 
 const app = createApp(App)
 // 创建pinia
@@ -11,4 +20,7 @@ const pinia = createPinia()
 // 安装pinia
 app.use(pinia)
 app.use(router)
+app.use(ElementPlus)
+// 安装自定义插件
+app.use(gloablComponent)
 app.mount('#app')
